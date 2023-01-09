8f73db162958828c68c15796cf02507b5d43a429
jakkumame <nkmrysyk0914@gmail.com>
2023-01-09 22:44:09 +0900

    g shared/components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SidenavComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]
