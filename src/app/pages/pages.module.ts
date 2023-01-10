060705ef3a47a038a2eff5bd221218dc8be57e15
jakkumame <nkmrysyk0914@gmail.com>
2023-01-10 10:39:44 +0900

    routing pages g pages

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,21 +1,30 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import {
+    ProductHistoryListingPageComponent
+} from './components/product-history-listing-page/product-history-listing-page.component';
+import {
+    ProductListingPageComponent
+} from './components/product-listing-page/product-listing-page.component';
+import {
+    ProductRegisteringPageComponent
+} from './components/product-registering-page/product-registering-page.component';
 import { SignInPageComponent } from './components/sign-in-page/sign-in-page.component';
-import { ProductListingPageComponent } from './components/product-listing-page/product-listing-page.component';
-import { ProductRegisteringPageComponent } from './components/product-registering-page/product-registering-page.component';
-import { ProductHistoryListingPageComponent } from './components/product-history-listing-page/product-history-listing-page.component';
-
-
+import { StockRegisteringPageComponent } from './components/stock-registering-page/stock-registering-page.component';
+import { PurchaseHistoryListingPageComponent } from './components/purchase-history-listing-page/purchase-history-listing-page.component';
 
 @NgModule({
   declarations: [
     SignInPageComponent,
     ProductListingPageComponent,
     ProductRegisteringPageComponent,
-    ProductHistoryListingPageComponent
+    ProductHistoryListingPageComponent,
+    StockRegisteringPageComponent,
+    PurchaseHistoryListingPageComponent
   ],
   imports: [
     CommonModule
-  ]
+  ],
 })
 export class PagesModule { }
